188e0bf1a8ac73ff9acf1b322ce1b9930e59e1b4
Matt B <mattbuckwell@gmail.com>
2023-12-08 19:36:55 +1300

    input field to search the api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // network request
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   // first argument in axios.get is the url we want to retrieve
   // ~ we have to wait for the response before we carry on with our code thats why
   //   we have the await keyword
@@ -17,9 +17,9 @@ const fetchData = async () => {
     params: {
       apikey: "1ca8cfc9",
       // s - used to perform a search request
-      // s: 'avengers'
+      s: searchTerm,
       // i - to fetch an individual movie by id
-      i: "tt0848228",
+      // i: "tt0848228",
     },
   });
 
@@ -27,4 +27,9 @@ const fetchData = async () => {
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+// input event activates when the text inside the input changes
+input.addEventListener("input", (event) => {
+  // this is how we have access to what the user has entered
+  fetchData(event.target.value);
+});
