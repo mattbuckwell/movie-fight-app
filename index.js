80dd2cc4d135cb0d8ed8c98cc4bae0c2ae9cd42c
Matt B <mattbuckwell@gmail.com>
2023-12-08 19:55:09 +1300

    delaying search input request to api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fetchData = async (searchTerm) => {
   // request
 
   // second argument is an object
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     // list out all the different query string paramaters that we want to pass along with the
     // request
 
@@ -28,8 +28,29 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector("input");
+// ID for the setTimeout
+let timeoutId;
+/*
+  When the user presses a key inside our input field, it will enter the function below. It
+  will check if timeoutID is defined which it won't be so will carry onto the setTimeout function
+  and set the timeoutID. With the next key pressed it will again enter the function below, but
+  this time timeoutId is defined and will call the clearTimeout function using the timeoutId to
+  stop the setTimeout currently being processed.
+
+  This will carry on until the user doesn't make anymore key presses and the setTimeout has had
+  time to execute and will call the fetchData using the users input onto the api to search for
+  a result
+*/
+const onInput = (event) => {
+  // check to see if timeoutId is defined
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    // this is how we have access to what the user has entered
+    fetchData(event.target.value);
+  }, 500);
+};
+
 // input event activates when the text inside the input changes
-input.addEventListener("input", (event) => {
-  // this is how we have access to what the user has entered
-  fetchData(event.target.value);
-});
+input.addEventListener("input", onInput);
